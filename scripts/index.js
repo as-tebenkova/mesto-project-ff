0ef292af59c84410f5247b341c5a2d840e1a5735
Aleksandra Tebenkova <as-tebenkova@yandex.ru>
2025-04-29 13:14:41 +0500

    Первая сдача ПР-4

--- scripts/index.js
+++ scripts/index.js
@@ -7,3 +7,21 @@
 // @todo: Функция удаления карточки
 
 // @todo: Вывести карточки на страницу
+
+const placesList = document.querySelector(".places__list");
+const cardTemplate = document.querySelector("#card-template").content;
+
+initialCards.forEach(function (element) {
+  const cardElement = cardTemplate.cloneNode(true);
+
+  cardElement.querySelector(".card__image").src = element.link;
+  cardElement.querySelector(".card__title").textContent = element.name;
+
+  const deleteButton = cardElement.querySelector(".card__delete-button");
+  deleteButton.addEventListener("click", function () {
+    const item = document.querySelector(".places__item");
+    item.remove();
+  });
+
+  placesList.append(cardElement);
+});
