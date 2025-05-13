1cca98d5a1e0675b988b7193d096a48bc23732c3
Aleksandra Tebenkova <as-tebenkova@yandex.ru>
2025-05-13 21:51:56 +0500

    Вторая сдача ПР-5

--- scripts/index.js
+++ scripts/index.js
@@ -1,27 +1,30 @@
 // @todo: Темплейт карточки
-
 // @todo: DOM узлы
-
 // @todo: Функция создания карточки
-
 // @todo: Функция удаления карточки
-
 // @todo: Вывести карточки на страницу
 
-const placesList = document.querySelector(".places__list");
-const cardTemplate = document.querySelector("#card-template").content;
+function deleteCard(event) {
+  const item = event.target.closest(".card");
+  item.remove();
+}
+
+function createCard(element) {
+  const cardTemplate = document.querySelector("#card-template").content;
+  const card = cardTemplate.cloneNode(true);
+
+  card.querySelector(".card__image").src = element.link;
+  card.querySelector(".card__image").alt = element.name;
+  card.querySelector(".card__title").textContent = element.name;
+
+  const deleteButton = card.querySelector(".card__delete-button");
+  deleteButton.addEventListener("click", deleteCard);
+
+  return card;
+}
 
 initialCards.forEach(function (element) {
-  const cardElement = cardTemplate.cloneNode(true);
-
-  cardElement.querySelector(".card__image").src = element.link;
-  cardElement.querySelector(".card__title").textContent = element.name;
-
-  const deleteButton = cardElement.querySelector(".card__delete-button");
-  deleteButton.addEventListener("click", function () {
-    const item = document.querySelector(".places__item");
-    item.remove();
-  });
-
+  const placesList = document.querySelector(".places__list");
+  const cardElement = createCard(element);
   placesList.append(cardElement);
 });
